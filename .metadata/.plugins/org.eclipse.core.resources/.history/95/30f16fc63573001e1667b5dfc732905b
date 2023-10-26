19ec0d4eebdda833fc0dd8eb8647e2134f24ddba
investsebin <sebinraphael7@gmail.com>
2023-10-26 10:53:12 +0530

    java utility updates

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/95/30f16fc63573001e1667b5dfc732905b
@@ -0,0 +1,14 @@
+package Generic_Library;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ListenerClass implements ITestListener {
+
+//	public void onTestFailure(ITestResult result) 
+//	{
+//		String screenshot =result.getMethod().getMethodName();
+//		String sc="screenshot"+
+//	}
+
+}
--- com.frame/src/main/java/Generic_Library/JavaUtility.java
+++ com.frame/src/main/java/Generic_Library/JavaUtility.java
@@ -1,5 +1,7 @@
 package Generic_Library;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class JavaUtility 
@@ -10,6 +12,18 @@ public class JavaUtility
 		int randomnumber=rand.nextInt(1000);
 //		System.out.println(randomnumber);
 		return randomnumber;
+	}
+	
+	public String getsysdatetime()
+	{
+		LocalDateTime currentDateTime=LocalDateTime.now();
+		DateTimeFormatter formatter=DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		String sc=currentDateTime.format(formatter);
+		System.out.println(sc);
+		String scr=sc.replaceAll(":","-");
+		return scr;
+		
+	
 	}
 
 }
